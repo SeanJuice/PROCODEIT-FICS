43ee3afbdc81c4c712ba8db74769aebb6d4a812a
Shaun <shaunmlax@gmail.com>
2021-09-01 19:15:01 +0200

    git

--- src/app/dashboard/Trainee/upload/upload.component.ts
+++ src/app/dashboard/Trainee/upload/upload.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LazyLoadScriptService } from '../../services/lazy-load-script.service';
+declare var $;
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
+  constructor(  private lazyLoadService: LazyLoadScriptService) { }
 
   ngOnInit() {
+    this.lazyLoadService.loadScript('/assets/jquery.min.js').subscribe((_) => {
+      console.log('Jquery is loaded!');
+      this.loadJS();
+    });
   }
 
+
+  loadJS(){
+    function readURL(input) {
+      if (input.files && input.files[0]) {
+
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+          $('.image-upload-wrap').hide();
+
+          $('.file-upload-image').attr('src', e.target.result);
+          $('.file-upload-content').show();
+
+          $('.image-title').html(input.files[0].name);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+
+      } else {
+        removeUpload();
+      }
+    }
+
+    function removeUpload() {
+      $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+      $('.file-upload-content').hide();
+      $('.image-upload-wrap').show();
+    }
+    $('.image-upload-wrap').bind('dragover', function () {
+        $('.image-upload-wrap').addClass('image-dropping');
+      });
+      $('.image-upload-wrap').bind('dragleave', function () {
+        $('.image-upload-wrap').removeClass('image-dropping');
+    });
+
+  }
 }
