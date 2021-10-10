3811f3552da1da6a510836c02f8354d4b5caa267
TravisMcMean <62015955+TravisMcMean@users.noreply.github.com>
2021-10-10 11:16:54 +0200

    Update set-availability.component.ts

--- src/app/dashboard/Practitioner/set-availability/set-availability.component.ts
+++ src/app/dashboard/Practitioner/set-availability/set-availability.component.ts
@@ -88,7 +88,7 @@ export class SetAvailabilityComponent implements OnInit {
 
   SetTimes() {
     Swal.fire({
-      title: !this.isReschedule ? 'Are you sure your availability is correct!' : 'Are you sure you want to reschedule?',
+      title: !this.isReschedule ? 'Are You Sure Your Availability Is Correct?' : 'Are you sure you want to reschedule?',
       icon: 'info',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
