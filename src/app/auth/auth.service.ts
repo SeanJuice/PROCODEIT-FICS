47f2d6f10442851a2ddb263bb30b077f9ab4b883
TravisMcMean <62015955+TravisMcMean@users.noreply.github.com>
2021-10-17 19:50:55 +0200

    .

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -17,10 +17,6 @@ import { FileUploadService } from '../shared/services/fileUpload.service';
 const KEY = 'FICSINF';
 
 const rootURL = 'https://localhost:44332/api/Access';
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 @Injectable({
   providedIn: 'root',
 })
